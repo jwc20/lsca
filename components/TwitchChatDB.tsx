4c4aca7a1d81e5a15cf6bfe15a8aa95dd3c62c93
jwc20 <choij2015@gmail.com>
2023-08-29 21:21:17 +0900

    Add message caching

--- components/TwitchChatDB.tsx
+++ components/TwitchChatDB.tsx
@@ -2,32 +2,72 @@
 
 import { useEffect, useState } from "react";
 import { firestore } from "../firebase/firebase";
-import { collection, getDocs, query, onSnapshot, orderBy } from "firebase/firestore";
+import { collection, getDocs, query, onSnapshot, orderBy, limit, startAfter, startAt } from "firebase/firestore";
+
+const CACHE_KEY = "twitch_chat_messages";
+const CACHE_DURATION = 60 * 60 * 1000; // 1 hour in milliseconds
 
 export default function TwitchChat({ channelName }: { channelName: string }) {
+  
     const [isOldestFirst, setIsOldestFirst] = useState(false);
     const [chatMessages, setChatMessages] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [itemsPerPage] = useState(69);
+    const [lastDocument, setLastDocument] = useState(null); 
 
     useEffect(() => {
         const orderDirection = isOldestFirst ? "asc" : "desc";
-        const q = query(
+
+        // Check cache first
+        const cachedData = localStorage.getItem(CACHE_KEY);
+        const cachedTimestamp = localStorage.getItem(`${CACHE_KEY}_timestamp`);
+        const currentTime = new Date().getTime();
+
+        if (cachedData && cachedTimestamp && (currentTime - Number(cachedTimestamp) < CACHE_DURATION)) {
+            setChatMessages(JSON.parse(cachedData));
+            return;
+        }
+        
+        let q = query(
             collection(firestore, "sodapoppin"),
-            orderBy("timestamp", orderDirection)
+            orderBy("timestamp", orderDirection),
+            limit(itemsPerPage)
         );
 
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        if (currentPage > 0) {
+            q = query(
+                collection(firestore, "sodapoppin"),
+                orderBy("timestamp", orderDirection),
+                startAfter(lastDocument),
+                limit(itemsPerPage)
+            );
+        }
+
+         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             const messages: any[] = [];
-            querySnapshot.forEach((doc: { data: () => any }) => {
+            querySnapshot.forEach((doc) => {
                 messages.push(doc.data());
             });
-            
+
+            // Update the state and cache the data
             setChatMessages(messages);
+            localStorage.setItem(CACHE_KEY, JSON.stringify(messages));
+            localStorage.setItem(`${CACHE_KEY}_timestamp`, String(currentTime));
         });
 
         return () => {
             unsubscribe();
         };
-    }, [isOldestFirst]);  
+    }, [isOldestFirst, currentPage]); 
+    
+
+    const handleNext = () => {
+        setCurrentPage(prev => prev + 1);
+    }
+
+    const handlePrev = () => {
+        setCurrentPage(prev => prev - 1);
+    }
 
     return (
         <div className="max-w-2xl mx-auto p-5 border rounded shadow-md">
@@ -48,6 +88,10 @@ export default function TwitchChat({ channelName }: { channelName: string }) {
                     </li>
                 ))}
             </ul>
+            <div className="mt-4">
+                {currentPage > 0 && <button onClick={handlePrev}>Previous</button>}
+                <button onClick={handleNext}>Next</button>
+            </div>
         </div>
     );
 }
