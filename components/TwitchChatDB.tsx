1a3164a8eeeba00046195c3e361bef6b641a33a1
jwc20 <choij2015@gmail.com>
2023-08-29 20:20:27 +0900

    Add chat ordering

--- components/TwitchChatDB.tsx
+++ components/TwitchChatDB.tsx
@@ -1,50 +1,53 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { firestore } from "../firebase/firebase";
 import { collection, getDocs, query, onSnapshot, orderBy } from "firebase/firestore";
 
-async function getChatMessages() {
-    const customers = await getDocs(collection(firestore, "zackrawrr"));
-    return customers;
-}
-
 export default function TwitchChat({ channelName }: { channelName: string }) {
-    const [isConnected, setIsConnected] = useState(false);
+    const [isOldestFirst, setIsOldestFirst] = useState(false);
     const [chatMessages, setChatMessages] = useState([]);
 
     useEffect(() => {
+        const orderDirection = isOldestFirst ? "asc" : "desc";
         const q = query(
-            collection(firestore, "zackrawrr"),
-            orderBy("timestamp", "desc") 
+            collection(firestore, "sodapoppin"),
+            orderBy("timestamp", orderDirection)
         );
+
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             const messages: any[] = [];
             querySnapshot.forEach((doc: { data: () => any }) => {
-                messages.unshift(doc.data()); // Add new data to the beginning of the array
+                messages.push(doc.data());
             });
             
             setChatMessages(messages);
         });
+
         return () => {
             unsubscribe();
         };
-    }, []);
-    
+    }, [isOldestFirst]);  
 
     return (
         <div className="max-w-2xl mx-auto p-5 border rounded shadow-md">
             <h1 className="text-center text-xl text-gray-700 border-b pb-4">Chat Messages for {channelName}</h1>
+            
+            <button onClick={() => setIsOldestFirst(!isOldestFirst)}>
+                {isOldestFirst ? 'Show Newest First' : 'Show Oldest First'}
+            </button>
+
             <ul className="divide-y divide-gray-200">
-                {[...chatMessages].reverse().map((message, index) => (
+                {chatMessages.map((message, index) => (
                     <li key={index} className="py-4">
                         <strong className="text-blue-500">{message.username}:</strong>
                         <p className="mt-2 text-lg">{message.chat_message}</p>
+                        <p className="mt-2 text-lg">{message.preprocessed_chat_message}</p>
                         <small className="text-gray-500">{new Date(message.timestamp).toLocaleString()}</small>
                         {message.is_toxic && <span className="ml-4 inline-block px-2 py-1 bg-red-500 text-white rounded">Toxic</span>}
                     </li>
                 ))}
             </ul>
         </div>
-    );    
+    );
 }
