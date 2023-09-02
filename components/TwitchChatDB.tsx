604cc2aaa4b3eb96de2a19a66f999b0839e4f148
jwc20 <choij2015@gmail.com>
2023-09-02 12:59:36 +0900

    Update chat component to get messages from the updated firestore db

--- components/TwitchChatDB.tsx
+++ components/TwitchChatDB.tsx
@@ -1,97 +1,83 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
-import { firestore } from "../firebase/firebase";
-import { collection, getDocs, query, onSnapshot, orderBy, limit, startAfter, startAt } from "firebase/firestore";
 
-const CACHE_KEY = "twitch_chat_messages";
-const CACHE_DURATION = 60 * 60 * 1000; // 1 hour in milliseconds
 
-export default function TwitchChat({ channelName }: { channelName: string }) {
-  
-    const [isOldestFirst, setIsOldestFirst] = useState(false);
-    const [chatMessages, setChatMessages] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [itemsPerPage] = useState(69);
-    const [lastDocument, setLastDocument] = useState(null); 
+import { useEffect, useState } from 'react';
+import { collection, getDocs, query, onSnapshot, orderBy, doc,getDoc , } from "firebase/firestore";
+import { firestore } from '../firebase/firebase';
+
+const ChatComponent = () => {
+    const [chats, setChats] = useState([]);
+
+    // useEffect(() => {
+    //     const fetchChats = async () => {
+    //         const chatPath = '/chats/sodapoppin/2023/09/02/00';
+    //         const messagesCollectionRef = collection(firestore, chatPath, 'chats');  // Accessing the sub-collection
+    
+    //         // Create a query against the collection
+    //         const q = query(messagesCollectionRef, orderBy("timestamp"));
+    
+    //         const querySnapshot = await getDocs(q);
+    //         let chatData = [];
+    //         querySnapshot.forEach((doc) => {
+    //             chatData.push(doc.data());
+    //         });
+    
+    //         setChats(chatData);
+    //     };
+    
+    //     fetchChats();
+    // }, []);
 
     useEffect(() => {
-        const orderDirection = isOldestFirst ? "asc" : "desc";
 
-        // Check cache first
-        const cachedData = localStorage.getItem(CACHE_KEY);
-        const cachedTimestamp = localStorage.getItem(`${CACHE_KEY}_timestamp`);
-        const currentTime = new Date().getTime();
+        const chatPath = '/chats/sodapoppin/2023/09/02/03';
+        const chatRef = doc(firestore, chatPath);
 
-        if (cachedData && cachedTimestamp && (currentTime - Number(cachedTimestamp) < CACHE_DURATION)) {
-            setChatMessages(JSON.parse(cachedData));
-            return;
-        }
-        
-        let q = query(
-            collection(firestore, "sodapoppin"),
-            orderBy("timestamp", orderDirection),
-            limit(itemsPerPage)
-        );
-
-        if (currentPage > 0) {
-            q = query(
-                collection(firestore, "sodapoppin"),
-                orderBy("timestamp", orderDirection),
-                startAfter(lastDocument),
-                limit(itemsPerPage)
-            );
-        }
-
-         const unsubscribe = onSnapshot(q, (querySnapshot) => {
-            const messages: any[] = [];
-            querySnapshot.forEach((doc) => {
-                messages.push(doc.data());
-            });
-
-            // Update the state and cache the data
-            setChatMessages(messages);
-            localStorage.setItem(CACHE_KEY, JSON.stringify(messages));
-            localStorage.setItem(`${CACHE_KEY}_timestamp`, String(currentTime));
+        const unsubscribe = onSnapshot(chatRef, (docSnapshot) => {
+            let chatData = docSnapshot.data().chats || [];  // Assuming the array field is named 'chats'
+    
+            // If you still want to sort them by timestamp:
+            chatData.sort((a, b) => (a.timestamp < b.timestamp ? 1 : -1));
+    
+            setChats(chatData);
         });
+        return () => unsubscribe();
+        
 
-        return () => {
-            unsubscribe();
-        };
-    }, [isOldestFirst, currentPage]); 
+
+        // if (docSnapshot.exists()) {
+        //     let chatData = docSnapshot.data().chats || [];  // Assuming the array field is named 'chats'
+
+        //     // Sort the chats array by timestamp
+        //     chatData.sort((a, b) => (a.timestamp > b.timestamp ? -1 : 1));
+
+        //     setChats(chatData);
+        // }
+    
+    
+        // fetchChats();
+    }, []);
     
 
-    const handleNext = () => {
-        setCurrentPage(prev => prev + 1);
-    }
-
-    const handlePrev = () => {
-        setCurrentPage(prev => prev - 1);
-    }
-
     return (
-        <div className="max-w-2xl mx-auto p-5 border rounded shadow-md">
-            <h1 className="text-center text-xl text-gray-700 border-b pb-4">Chat Messages for {channelName}</h1>
-            
-            <button onClick={() => setIsOldestFirst(!isOldestFirst)}>
-                {isOldestFirst ? 'Show Newest First' : 'Show Oldest First'}
-            </button>
-
-            <ul className="divide-y divide-gray-200">
-                {chatMessages.map((message, index) => (
-                    <li key={index} className="py-4">
-                        <strong className="text-blue-500">{message.username}:</strong>
-                        <p className="mt-2 text-lg">{message.chat_message}</p>
-                        <p className="mt-2 text-lg">{message.preprocessed_chat_message}</p>
-                        <small className="text-gray-500">{new Date(message.timestamp).toLocaleString()}</small>
-                        {message.is_toxic && <span className="ml-4 inline-block px-2 py-1 bg-red-500 text-white rounded">Toxic</span>}
+        <div className="bg-gray-800 p-6 rounded-lg max-w-3xl mx-auto mt-12">
+            <h2 className="text-white text-center mb-4">Chats</h2>
+            <ul className="divide-y divide-gray-600">
+                {chats.map((chat, index) => (
+                    <li key={index} className={`py-2 px-3 ${index % 2 === 0 ? 'bg-gray-700' : 'bg-gray-800'}`}>
+                        {/* <strong className="text-green-400">{chat.username}:</strong> */}
+                        <strong className="text-green-400">{chat.username ? chat.username + ':' : ''}</strong>
+                        {/* {chat.username && <span className="text-white ml-2">{chat.username}:</span>} */}
+                        <span className="text-white ml-2">{chat.chat_message}</span>
+                        <span className="text-gray-400 text-sm ml-4">{new Date(chat.timestamp).toLocaleString()}</span>
                     </li>
                 ))}
             </ul>
-            <div className="mt-4">
-                {currentPage > 0 && <button onClick={handlePrev}>Previous</button>}
-                <button onClick={handleNext}>Next</button>
-            </div>
         </div>
     );
+    
+    
 }
+
+export default ChatComponent;
