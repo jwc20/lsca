ed86c8ac39b3f2cb3031ae822f77267f95a9eb87
jwc20 <choij2015@gmail.com>
2023-09-16 11:47:51 +0900

    epic troll

---
+++ app/maym/page.tsx
@@ -0,0 +1,28 @@
+
+
+export default function Maym() {
+
+    return (
+        <div>
+            {/* center the div tag in the middle of the screen both horizontally and vertically*/}
+            <div className="flex justify-center items-center h-screen">
+                <div className="text-6xl uppercase font-black">
+                <pre>
+                <code>{`
+    Me    O 
+         /|\\         Your Mom 
+          |_  ___O
+         / \\ /\\  \\  
+
+           `}
+           </code>
+           </pre>
+     
+            </div>
+
+        </div>
+        </div>
+
+
+    )
+}
